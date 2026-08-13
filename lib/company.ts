d96e3cb457d1739c8633cd286cc1c1ab62fec551
contactgamesignals <contact.gamesignals@gmail.com>
2026-08-13 02:34:25 +0200

    Add Lumino Games operator details

---
+++ lib/company.ts
@@ -0,0 +1,9 @@
+export const COMPANY = {
+  legalName: "Lumino Games sp. z o.o.",
+  registeredAddress: "ul. Ujastek 1, 31-752 Kraków, Poland",
+  krs: "0000910452",
+  nip: "6762600090",
+  regon: "389433660",
+  productName: "GameSignal",
+  supportEmail: "contact.gamesignals@gmail.com",
+} as const;
