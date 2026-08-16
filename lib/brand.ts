5fa69cf294b6a43e32f33eeb59882eac55d776ac
contactgamesignals <contact.gamesignals@gmail.com>
2026-08-16 23:19:57 +0200

    Add Who Plays My Game brand constants

---
+++ lib/brand.ts
@@ -0,0 +1,10 @@
+export const BRAND = {
+  name: "Who Plays My Game",
+  siteUrl: "https://www.whoplaysmygame.com",
+  supportEmail: "whoplaysmygame@gmail.com",
+} as const;
+
+export const LEGACY_BRAND = {
+  name: "GameSignal",
+  supportEmail: "contact.gamesignals@gmail.com",
+} as const;
