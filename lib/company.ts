99fed4dbf75f006a754b0c49748346710480e356
contactgamesignals <contact.gamesignals@gmail.com>
2026-08-17 18:49:20 +0200

    Centralize structured company address fields

--- lib/company.ts
+++ lib/company.ts
@@ -1,6 +1,11 @@
 export const COMPANY = {
   legalName: "Lumino Games sp. z o.o.",
   registeredAddress: "ul. Kazimierza Morawskiego 5/127, 30-102 Kraków, Małopolskie, Poland",
+  streetAddress: "ul. Kazimierza Morawskiego 5/127",
+  postalCode: "30-102",
+  city: "Kraków",
+  region: "Małopolskie",
+  countryCode: "PL",
   krs: "0000910452",
   nip: "6762600090",
   regon: "389433660",
